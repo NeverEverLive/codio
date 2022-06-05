c4255718eeafa7a5587d217808751fc96727fa00
Nikolay Pushkarev <NeverEverLives@gmail.com>
2022-06-05 19:57:30 +0300

    Add nested relationships

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,13 +1,37 @@
-from codecs import lookup
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
-from blog.models import Post, Tag
+from blog.models import Post, Tag, Comment
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["first_name", "username"]
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    creator = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ["id", "creator", "content", "modified_at", "created_at"]
+        read_only = ["modified_at", "created_at"]
+
+
+class TagField(serializers.SlugRelatedField):
+    def to_internal_value(self, data):
+        try:
+            return self.get_queryset().get_or_create(value=data.lower())[0]
+        except (TypeError, ValueError):
+            self.fail(f"Tag value {data} is invalid")
 
 
 class PostSerializer(serializers.ModelSerializer):
-    tags = serializers.SlugRelatedField(
-        slug_field="value", many=True,
+    tags = TagField(
+        slug_field="value", 
+        many=True,
         queryset=Tag.objects.all()
     )
 
@@ -17,13 +41,31 @@ class PostSerializer(serializers.ModelSerializer):
         lookup_field="username"
     )
 
+    author = UserSerializer(read_only=True)
+
     class Meta:
         model = Post
         fields = '__all__'
         readonly = ['modified_at', 'created_at']
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["first_name", "username"]
+class PostDetailSerializer(PostSerializer):
+    comments = CommentSerializer(many=True)
+
+    def update(self, instance, validated_data):
+        comments = validated_data.pop("comments")
+
+        print(validated_data)
+        print(comments)
+
+        instance = super(PostDetailSerializer, self).update(instance, validated_data)
+
+        for comment_data in comments:
+            if comment_data.get("id"):
+                continue
+            comment = Comment(**comment_data)
+            comment.creator = self.context["request"].user
+            comment.content_object = instance
+            comment.save()
+
+        return instance
